671871fecf0d5de5584cfbcb6eb69100a7899c46
yl-zeng <ylinzeng69@gmail.com>
2017-06-26 22:37:31 -0400

    Wrap up add Product

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -5,8 +5,10 @@ import com.emusicstore.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
 import java.util.List;
@@ -66,6 +68,13 @@ public class HomeController {
         model.addAttribute(product);
 
         return "addProduct";
+    }
+
+    @RequestMapping(value = "/admin/productInventory/addProduct", method = RequestMethod.POST)
+    public String addProductPost(@ModelAttribute("product") Product product){
+        productDao.addProduct(product);
+
+        return "redirect:/admin/productInventory";
     }
 
 
