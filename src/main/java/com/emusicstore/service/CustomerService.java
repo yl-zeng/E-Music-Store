97ea6f9af0075349636bc653630b793defd5e6d0
yl-zeng <ylinzeng69@gmail.com>
2017-07-09 22:03:11 -0400

    Create CustomerService interface and Impl class

---
+++ src/main/java/com/emusicstore/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.emusicstore.service;
+
+import com.emusicstore.model.Customer;
+
+import java.util.List;
+
+/**
+ * Created by genji on 7/9/2017.
+ */
+public interface CustomerService {
+
+    void addCustomer(Customer customer);
+
+    Customer getCustomerById(int customerId);
+
+    List<Customer> getAllCustomer();
+
+
+}
---
+++ src/main/java/com/emusicstore/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,32 @@
+package com.emusicstore.service.impl;
+
+import com.emusicstore.dao.CustomerDao;
+import com.emusicstore.model.Customer;
+import com.emusicstore.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by genji on 7/9/2017.
+ */
+
+@Service
+public class CustomerServiceImpl implements CustomerService{
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    public void addCustomer(Customer customer){
+        customerDao.addCustomer(customer);
+    }
+
+    public Customer getCustomerById(int customerId){
+        return customerDao.getCustomerById(customerId);
+    }
+
+    public List<Customer> getAllCustomer(){
+        return customerDao.getAllCustomer();
+    }
+}
