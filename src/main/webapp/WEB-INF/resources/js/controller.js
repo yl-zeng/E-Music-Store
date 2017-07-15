6249628792bafde3153956aaaabf41548250d796
yl-zeng <ylinzeng69@gmail.com>
2017-07-15 17:15:56 -0400

    fix spring security default log in direct

--- src/main/webapp/WEB-INF/resources/js/controller.js
+++ src/main/webapp/WEB-INF/resources/js/controller.js
@@ -5,7 +5,6 @@ cartApp.controller("cartCtrl",function ($scope,$http){
     $scope.refreshCart = function(){
         $http.get('/rest/cart/' + $scope.cartId).success(function (data){
            $scope.cart = data;
-           console.log("cart: ",$scope.cart);
         });
     };
 
@@ -23,28 +22,28 @@ cartApp.controller("cartCtrl",function ($scope,$http){
         $http.put("/rest/cart/add/" + productId).success(function(data){
             $scope.refreshCart($http.get("/rest/cart/cartId"));
             alert("product successfully added to cart");
-        })
+        });
     };
 
     $scope.removeFromCart = function(productId){
         $http.put("/rest/cart/remove/" + productId).success(function(){
             alert("product successfully removed");
-        })
+        });
     };
 
     $scope.clearCart = function(cartId){
         $http.delete("/rest/cart/" + cartId).success(function(){
             $scope.refreshCart();
-        })
+        });
     };
 
     $scope.calGrandTotal = function () {
         var grandTotal = 0;
 
-        for(var i = 0; i<$scope.cartItems.length; i++){
-            grandTotal += $scope.cartItem[i].totalPrice;
-        }
+        for(var i = 0; i<$scope.cart.cartItems.length; i++){
+            grandTotal += $scope.cart.cartItems[i].totalPrice;
+        };
 
         return grandTotal;
-    }
+    };
 });
