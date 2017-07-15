f794bf1e0f8a0ade2ac9ccd3c76e09b0e341673d
yl-zeng <ylinzeng69@gmail.com>
2017-07-15 17:17:03 -0400

    Create CustomerOrderDao and Impl

---
+++ src/main/java/com/emusicstore/dao/CustomerOrderDao.java
@@ -0,0 +1,12 @@
+package com.emusicstore.dao;
+
+import com.emusicstore.model.CustomerOrder;
+
+/**
+ * Created by genji on 7/15/2017.
+ */
+public interface CustomerOrderDao {
+
+    void addCustomerOrder(CustomerOrder customerOrder);
+
+}
